fd3bb689073d6b70340f9d77d0c2aaceea291969
Your Name <delftkarmilan@gmail.com>
2021-01-19 08:15:20 +0530

    hotte first comit

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotte</title>
+
+<link rel="stylesheet" href="css/bootstrap5.css">
+<!-- <link rel="stylesheet" href="css/style.css"> -->
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+    <div class="bgimg">
+        <header>
+                    <nav class="navbar navbar-dark " style="">
+        
+                        <div class="container-fluid">
+                            <img src="images/logo1.png" alt="">
+                        
+                            <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Type anything what you want" aria-label="Search">
+                            <button class="btn btn-dark" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </nav>       
+        </header>   
---
+++ index.php
@@ -0,0 +1,358 @@
+<?php include 'includes/header.php';?>
+
+
+        <div class="homebnr">
+            <div class="row" style="">
+            <div class="col-6" style="text-align:center;">
+            
+                <h2 style="color:white;">Take <span style="color:red">Food</span><br> & <span style="color:red">Let's Go</span> </h2>
+                <p style="color:white; margin-left: 170px;">Have any food on your mind? <span style="color:red">just order now</span> </p>
+                <button class="btntodaymenu" type="button">Today's Menu >></button><br>
+
+                <div class="circle">
+                <span class="dot" style="height: 30px; width: 30px; background-color: orange; margin-right: -69px; margin-bottom: -3rem;" ></span>
+                <span class="dot" style="height: 60px; width: 60px; background-color: #1dff1df2; margin-bottom: -7rem;" ></span>
+                <span class="dot" style="height: 20px; width: 20px; background-color: red;     margin-bottom: -5rem;"></span>
+                </div>
+
+            </div>
+
+            <div class="col-6">
+                <img src="images/burger.png" alt="" srcset="">
+            </div>
+            </div>
+        </div>
+    </div>
+
+
+<!-- today special start -->
+
+<div class="row">
+    <div class="col-2"></div>
+    <div class="col-4">
+    <div class="ts" style="margin: 20px;">
+        <img src="images/snaks11.png">
+    </div>    
+
+    <div class="chipsimg">
+    <img src="images/chips.png">
+    </div>
+
+    </div>
+    <div class="col-4">
+
+    <div class="tss">
+        <h2>Today's Special <br><span style="color:orangered;">Surprise For You</span></h2>
+        <p>It is a long established fact that a will distracted readable content of a page when looking at its the layout using lorem ipsum is that it has a normal distribution 
+          <span style="color:orangered">as opposed to using making it look like</span>  
+        </p>
+
+        <div class="offer">
+            
+        
+            
+            
+
+            <div class="row">
+                <div class="col-4"><p>$<span style="font-size: x-large;">89.00</span></p></div>
+
+                <div class="col-4">
+                    
+                        <div class="tri-shape">
+                        <span> +<br>02<br>- </span>          
+                        </div>
+                    
+                </div>
+                
+                <div class="col-4">
+                    <input type="button" value="Claim Now" class="claimbtn">
+                </div>
+            </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-2"></div>
+</div>
+
+<!-- today special end-->
+
+<!-- step to order start -->
+
+    <div class="row">
+        <div class="col-2"></div>
+           
+                <div class="col-8">
+                
+                    <div class="stepbnr">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="food">
+                                    <i class="fas fa-5x fa-hamburger"></i>
+                                    <div class="choose-food">
+                                        <h4>Choose Food</h4>
+                                        <p>It is a long establed fact will distracted readable looking at layou</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="order">
+                                <i class="fas fa-5x fa-cart-arrow-down"></i>
+                                    <div class="place-order">
+                                        <h4>Place Order</h4>
+                                        <p>It is a long establed fact will distracted readable looking at layou</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <div class="receive">
+                                <i class="fas fa-5x fa-truck"></i>
+                                    <div class="receive-go">
+                                        <h4>Receive & Go</h4>
+                                        <p>It is a long establed fact will distracted readable looking at layou</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                </div>                
+            
+        <div class="col-2"></div>        
+    </div><br>
+
+
+<!-- step to order end -->
+
+<!-- dark banner start -->
+
+<div class="darkbnr">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-10">
+            <div class="bg-burger">
+                <div class="row">
+
+                    <div class="col-4">
+                        <h1 style="color:white;">Today's <span style="color:orange;">Offers</span></h1>
+                        <p style="color:white;">It is a long establed fact will distracted readable looking at layou</p><br>
+                       
+                        <div class="vertical-bar">
+                            <p>There are many variation passages of lorem ipsum available, but the majority have suffered 
+                                alteration in form, by injected humour randomised words which don't look even sightly believable.
+                            </p>
+                        </div><br>
+
+                        <input type="button" class="orderbtn" value="Order Now">
+
+                        <p style="color:orangered; margin-top: -40px;">$<span style="font-size: x-large;">89.00  </span>
+
+                        </p>
+                        
+
+                    </div>
+
+                    <div class="col-4">
+                        <img src="images/cirfry.png" style="height: 150px;">
+                    </div>
+
+                    <div class="col-4"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div><br>
+
+<!-- dark banner end -->
+
+<!-- satisfied customer start -->
+
+<div class="row">
+    <div class="col-2"></div>
+
+    <div class="col-8">
+
+        <h1 style="text-align:center">Some of Our <br> Satisfied <span style="color:orangered">Customer</span></h1>
+
+        <div class="row">
+
+            <div class="col-2">  
+                <div class="customer-left">
+
+                    <img src="images/cmtajith.jpg" style="height: 50px; width: 50px; border-radius: 10px; transform: rotate(20deg);">
+                    <br><br><br><br><br><br><br><br><br><br>
+                    <img src="images/cmtajith.jpg" style="height: 50px; width: 50px; border-radius: 10px; transform: rotate(20deg);">
+
+                </div>
+            </div>
+
+            <div class="col-8">
+
+
+                <div class="customer-comments">
+
+                    <div class="inner-cicle">
+                        <div class="cmt-card">
+                            <img src="images/cmtajith.jpg" style="height: 50px; width: 50px; border-radius: 10px;">
+                        </div>                        
+                    </div>
+
+                        <p>
+                            If you are going to use a passage of cren you need to isn't anything embarrassing hidden 
+                            in the middle of text generators internet tend repeat predefined chunks as necessary making
+                            this is the first true generator on the internet <br>                           
+                        </p>
+                          
+                        <span style="text-align:center; color:black;">
+                            <h5>Ajith Kumar <br>Happy Customer</h5>
+                        </span><br>
+
+                </div>
+            </div>
+
+            <div class="col-2">
+                <div class="customer-right">
+
+                    <img src="images/cmtajith.jpg" style="height: 50px; width: 50px; border-radius: 10px; transform: rotate(-20deg);">
+                    <br><br><br><br><br><br><br><br><br><br>
+                    <img src="images/cmtajith.jpg" style="height: 50px; width: 50px; border-radius: 10px; transform: rotate(-20deg);">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-2"></div>
+</div> <br>
+
+<!-- satisfied customer start -->
+
+<!-- orange banner start -->
+
+<div class="orangebnr">
+    <div class="row">
+        <div class="col-2"></div>
+
+        <div class="col-8">
+            <div class="row">
+                <div class="col-6">
+                    <div class="dot-bar">
+                        <h2>We Make Tasty <span style="color:black;">Food Everyday</span></h2>
+                        <p>If you are going to use a passage of you need to be sure there isn't anything embarrassing hidden
+                            in the middle of text.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="play-intro">
+                        <i class="far fa-3x fa-play-circle"></i>
+                        <h2>20 Minutes Intro</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-2"></div>
+    </div><br>
+</div><br>
+
+<!-- orange banner end -->
+
+<!-- contact start -->
+
+<div class="row">
+    <div class="col-2"></div>
+
+    <div class="col-8">
+        <div class="row">
+            <div class="col-4">
+                <img src="images/logo1.png" alt="" srcset="">
+                <p>If you are going to use a passage of you need to be sure there isn't anything hidden
+                   in the middle of text
+                </p>
+
+                <p style="color:red;">If you are going to use a passage of you need to be sure there isn't</p>
+
+                <a href="facebook.com"><i class="fab fa-facebook-square"></i></a>
+                <a href="pinterest.com"><i class="fab fa-pinterest-square"></i></a>
+                <a href="twitter.com"><i class="fab fa-twitter-square"></i></a>
+
+            </div>
+
+            <div class="col-4">
+                <h2>Contact Us</h2>
+
+                <div class="contact"><br>
+                    <i class="fas fa-2x fa-phone-square-alt"></i>
+                    <p>+880 659 468 <br>02 697 86456 </p>
+
+                    <i class="fas fa-2x fa-envelope-square"></i>
+                    <p>support@gmail.com <br>hotte24@gmail.com</p>
+
+                    <i class="fas fa-2x fa-map-marker-alt"></i>
+                    <p>855 Road, Brooklyn<br>Street New York 600,<br>Wisconsin(WI)</p>
+                </div>
+
+            </div>
+            <div class="col-4">
+            <div class="hours">
+                <h3>Opening Hours</h3>
+            <table>
+            <div class="inner-circle">
+                <tr>
+                    <td>Sunday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td style="color:red;">&nbsp&nbsp&nbspClossing Day</td>
+                </tr>
+                <tr>
+                    <td>Monday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+                <tr>
+                    <td>Tuesday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+                <tr>
+                    <td>Wednesday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+                <tr>
+                    <td>Thursday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+                <tr>
+                    <td>Friday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+                <tr>
+                    <td>Saturday</td>
+                    <td>&nbsp&nbsp&nbsp------</td>
+                    <td>&nbsp&nbsp&nbsp8AM-10PM</td>
+                </tr>
+
+                </div>
+
+            </table>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-2"></div>
+</div><br>
+
+<!-- contact end -->
+
+<?php include 'includes/footer.php';?>
